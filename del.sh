2e6e954d0f22ffddd0fdeac0dd55d54f31f66c6f
Maxim <maxim.bassalayev@gmail.com>
2022-05-22 00:40:55 +0300

    better

--- del.sh
+++ del.sh
@@ -1,1 +1,2 @@
-rm lab.aux lab.bbl lab.bcf lab.blg lab.fls lab.fdb_latexmk lab.glo lab.ist lab.log lab.out lab.pdf lab.run.xml
+rm lab.aux lab.bbl lab.bcf lab.blg lab.fls lab.fdb_latexmk lab.glo lab.ist lab.log lab.out lab.pdf lab.run.xml lab.toc
+rm -rf _minted-lab 
